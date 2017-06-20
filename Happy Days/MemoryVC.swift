65f255cd35de3c1e396b8297aaf799710a745e4e
akilkumar28 <akil.psn@gmail.com>
2017-06-20 17:30:17 +0530

    permissions tweekss and added some photo stuff

--- Happy Days/MemoryVC.swift
+++ Happy Days/MemoryVC.swift
@@ -7,12 +7,77 @@
 //
 
 import UIKit
+import AVFoundation
+import Speech
+import Photos
 
 class MemoryVC: UICollectionViewController {
+    
+    
+    var memories = [URL]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    
+    func checkPermission() {
+        
+        let photoAuthorized = PHPhotoLibrary.authorizationStatus() == .authorized
+        let audioAuthorized = AVAudioSession.sharedInstance().recordPermission() == .granted
+        let transcriberPermission = SFSpeechRecognizer.authorizationStatus() == .authorized
+        
+        let permission = photoAuthorized && audioAuthorized && transcriberPermission
+        
+        
+        if permission == false {
+            if let vc = storyboard?.instantiateViewController(withIdentifier: "firstRun") {
+                navigationController?.present(vc, animated: true, completion: nil)
+            }
+        }
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        checkPermission()
+    }
+    
+    
+    func documentDirectories() -> URL {
+        
+        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = path[0]
+        return documentDirectory
+        
+    }
+    
+    func loadMemories() {
+        
+        memories.removeAll()
+        
+        
+        guard let files = try? FileManager.default.contentsOfDirectory(at: documentDirectories(),includingPropertiesForKeys: nil,options:[]) else{
+            return
+        }
+        
+        for file in files {
+            
+            let fileName = file.lastPathComponent
+            
+            if fileName.hasSuffix(".thumb"){
+                
+                let noExtension = fileName.replacingOccurrences(of: ".thumb", with: "")
+                
+                let memoryPath = documentDirectories().appendingPathComponent(noExtension)
+                
+                memories.append(memoryPath)
+            }
+            
+        }
+        
+        collectionView?.reloadSections(IndexSet(integer: 1))
+        
     }
 
 
